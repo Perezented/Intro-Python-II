510b384ceb1c57b27b0e3f4cac11b33ae4d48804
Michael Perez <mperez6473@gmail.com>
2020-07-09 14:42:47 -0500

    Added an items property to Rooms. Have a print items function. Changed descriptions and rommsList to be able to hold items.

--- src/adv.py
+++ src/adv.py
@@ -6,29 +6,29 @@ from item import Item
 # Declare all the rooms
 
 roomsList = {
-    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons", []),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", []),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", []),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("Treasure Chamber", """You've found the long-lost Brinks brand treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers.The only exit is to the south. """, [
+    Item("Key", "By the looks of it, it's a Master brand key. That's strange. Who has a Master brand lock around here? ")
+]),
 }
 
 
 # Room(room)
-# setting all the room names to an array
+# setting all the room names to an object
 room = {}
-# and all their descriptions with text in another array
-roomDesc = []
 # made a dict to hold all the locations and their descriptions
 for k, v in roomsList.items():
     #set the key and values from roomsList into the dict
--- src/room.py
+++ src/room.py
@@ -2,10 +2,15 @@
 # description attributes.
 class Room:
     # Class for room that has a name and description
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
+        # add a property to the rooms where they can hold items.
+        self.items = items
     # string for it as well
     def __str__(self):
         return f"{self.name}."
-    # ability to move directions
+    # print the items of the room with a function.
+    def printItems(self):
+        for i, items in enumerate(self.items):
+            print(f"{i + 1} - {items}")
